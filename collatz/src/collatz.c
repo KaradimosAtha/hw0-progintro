9d461440a72a31192910b3ad439e1cfe9e0073a6
Karadimoss <sdi2300062@ThanasisLaptop>
2023-11-01 19:50:17 +0200

    Solution For collatz

--- collatz/src/collatz.c
+++ collatz/src/collatz.c
@@ -1,22 +1,22 @@
-//This program finds the maximum length of the collatz conjecture in an specific border of nu:mbers 
+//This program finds the maximum length of the collatz conjecture in an specific border of nummbers 
 
 #include <stdio.h>
 #include <stdlib.h>
 
-//We include the stdio.h library for the function printf and the stdlib.h for the faction atoi 
+/*Bazoume to library stdio.h prokimenou na xrisimopihsoume th sinartisi printf , kai to stdlib.h gia na xrisimopoihsoumetin sinartish atoi */ 
 
 int main(int argc , char**argv) {
 long long int N ;
 
-/*We use long long int because if we give for example the border of 10 1000000000 with int the program will return a negative number , because as the program does the calculations it can surpass 2.147.483.647 which is the maximum possible number an integer can take*/
+/*Epilegoume na xrhsimopoihsoume long long int oste na min ginei iperxilisi tou akaireou N , etsi bazoume san tipo long long int o opoios anaparistate me perisotera bytes sthn mnimi kai exei megalitero euros timon */
 
 if (argc!=3){
 printf("Program needs to be called as `./prog border1 border2`\n");
 return 1; 
 }
-
-//The border1 is the first number the user gives and border2 the second one
-//We begin max with -1 and we add a counter in order to count the collatz length of a number
+//To border1 einai to proto orisma kai to border2 to deutero 
+//Jekiname to max=-1 h opoia einai mia xamili timi , sigoureuontas oti to max tha alajei apo tin proti epanalhch 
+//Xrisimopoihoume enan metriti count o opoios tha metraei gia kathe arithmo to mikos collatz tou 
 
 int border1 = atoi(argv[1]);
 int border2 = atoi(argv[2]);
@@ -24,7 +24,7 @@ int max = -1 ;
 int count;
 int i ;
 
-//If the user gives a negative number the program has to return 0 else it starts calculating the lenght of this number
+//An o user dosei arithmo < 0 (Diladi border1<0) tote to programma girnaei 0 
 if (border1< -100000000 || border2> 100000000 ) {
  printf("You have to give  integer numbers that belong to this border [ -100000000, 100000000] \n") ;
 return 1;
@@ -33,13 +33,15 @@ if(border1 > border2 ) {
 printf("border1 has to be < from border2\n");
 return 1;
 }
+/* Exo valei kai kapoies alles periptoseis oi opoioes elenxoun oti to border1 < border2 kai oti o user dinei times apo
+[-100000000 , 100000000] */
 
 if (border1<0){
 printf("%d\n" , 0 ) ;
 return 1 ; }
 else {
 
-//We use the loop for and while in order to calculate the lenght
+//Xrisimopoioume ena for loop kai ena while loop 
 //H epanaliψh for einai gia na jekinisei apo to proto border kai na ftasei sto deutero aujanontas kathe fora to i kata 1 
 //Eno h epanalhch while einai gia kathe enan apo tous arithmous na ipologizei to mikos collazt
 //To count jekinaei apo 1 gia na metraei kai ton iparxon arithmo kai to exoume mesa sthn for gia na metraei gia kathe arithmo , ara prepei na janaginete 1 otan allazoume arithmo
