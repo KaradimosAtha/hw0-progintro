14398911779332c4d86c45c5630d8662e2120902
Karadimoss <sdi2300062@ThanasisLaptop>
2023-11-05 18:36:21 +0200

    Changes on collatz source file

--- collatz/src/collatz.c
+++ collatz/src/collatz.c
@@ -3,20 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Bazoume to library stdio.h prokimenou na xrisimopihsoume th sinartisi printf , kai to stdlib.h gia na xrisimopoihsoumetin sinartish atoi */ 
+/*Βαζουμε τη βιβλιοθηκη stdio.h προκειμενου να χρησιμοποιησουμε τη συναρτηση printf , και το stdlib.h  για να χρησιμοποιησουμε τη συναρτηση atoi  */ 
 
 int main(int argc , char**argv) {
 long long int N ;
 
-/*Epilegoume na xrhsimopoihsoume long long int oste na min ginei iperxilisi tou akaireou N , etsi bazoume san tipo long long int o opoios anaparistate me perisotera bytes sthn mnimi kai exei megalitero euros timon */
+/*Επιλεγουμε να χρησιμοποιησουμε long long int  ωστε να μην γινει υπερχηλειση του ακεραιου N , ετσι βαζουμε σαν τυπο  long long int o οποιος αναπαρισταται με περισσοτερα bytes στη μνημη  και εχει μεγαλυτερο ευρος τιμων  */
 
 if (argc!=3){
 printf("Program needs to be called as `./prog border1 border2`\n");
 return 1; 
 }
-//To border1 einai to proto orisma kai to border2 to deutero 
-//Jekiname to max=-1 h opoia einai mia xamili timi , sigoureuontas oti to max tha alajei apo tin proti epanalhch 
-//Xrisimopoihoume enan metriti count o opoios tha metraei gia kathe arithmo to mikos collatz tou 
+
+//Το border1 ειναι το πρωτο ορισμα (το low ) και το border2 το δευτερο (high)
+
+//Ξεκιναμε το max=-1 που ειναι μια χαμηλη τιμη  προκειμενου να αλλαξει μεσα στο loop
+
+//Χρησιμοποιουμε εναν μετρητη  count ο οποιος θα μετραει  για καθε αριθμο το μηκος collatz του 
 
 int border1 = atoi(argv[1]);
 int border2 = atoi(argv[2]);
@@ -24,7 +27,6 @@ int max = -1 ;
 int count;
 int i ;
 
-//An o user dosei arithmo < 0 (Diladi border1<0) tote to programma girnaei 0 
 if (border1< -100000000 || border2> 100000000 ) {
  printf("You have to give  integer numbers that belong to this border [ -100000000, 100000000] \n") ;
 return 1;
@@ -33,42 +35,43 @@ if(border1 > border2 ) {
 printf("border1 has to be < from border2\n");
 return 1;
 }
-/* Exo valei kai kapoies alles periptoseis oi opoioes elenxoun oti to border1 < border2 kai oti o user dinei times apo
+/* Εχω βαλει και καποιες αλλες περιπτωσεις  οι οποιες  ελεγχουν οτι το border1 < border2 και οτι ο χρηστης δινει τιμες απο 
 [-100000000 , 100000000] */
 
-if (border1<0){
+if (border1<0){        //Αν ο χρηστης δωσει αριθμο < 0 (Δηλαδη border1<0) τοτε το προγραμμα γυρναει  0
 printf("%d\n" , 0 ) ;
 return 1 ; }
 else {
 
-//Xrisimopoioume ena for loop kai ena while loop 
-//H epanaliψh for einai gia na jekinisei apo to proto border kai na ftasei sto deutero aujanontas kathe fora to i kata 1 
-//Eno h epanalhch while einai gia kathe enan apo tous arithmous na ipologizei to mikos collazt
-//To count jekinaei apo 1 gia na metraei kai ton iparxon arithmo kai to exoume mesa sthn for gia na metraei gia kathe arithmo , ara prepei na janaginete 1 otan allazoume arithmo
+//Χρησιμοποιουμε ενα for loop και ενα while loop
+ 
+//Το for loop  ειναι για να ξεκινησει απο το πρωτο border και να φτασει στο δευτερο αυξανοντας καθε φορα το  i κατα  1 
+
+//Ενω το while loop ειναι για καθε εναν απο τους αριθμους να υπολογιζει το μηκος collatz του και τερματιζει οταν ο αριθμος μετα απο πραξεις καταληξει στο 1
+
+//Το count ξεκιναει με  1 για να μετραει και τον υπαρχον αριθμο και το εχουμε μεσα στη for για να μετραει για καθε αριθμο , αρα πρεπει να ξαναγινεται  1 οταν αλλαζουμε αριθμο 
 
 for (i = border1; i <= border2; ++i) 
 {
-N=i ; 
-count = 1;
+N=i ;     //Βαζουμε N=i γιατι εαν χρησιμοποιησω μονο μια μεταβλητη , η τιμη της θα αλλαζει συνεχως μεσα στη for με αποτελεσμα οταν γυρναει πισω στη συνθηκη 
+count = 1;     //να γυνονται απειρες επαναλαψεις αφου δεν θα φτασει ποτε στο border2   
 if (N==1) {
 count = 1 ;
 } 
 while (N!=1) {
 
-if ( N % 2 == 0 ) { 
+if ( N % 2 == 0 ) {   // Εαν ο αριθμος ειναι αρτιος τοτε το Ν γινεται N/2
 N = N / 2 ;
  } 
 else { 
-N = (3 * N)  +1 ;
+N = (3 * N)  +1 ;    // Αλλιως (που σημαινει οτι ειναι περιττος) γινεται 3*Ν +1 
 }
 
-//Ean o arithmos einai zigos tote to N ginete N/2 allios ean einai perittos to N ginete 3*N +1
-
-count = count + 1 ; 
+count = count + 1 ;  
 } 
-if ( count > max ) { 
-max = count ; 
-} 
+if ( count > max ) {   
+max = count ;                
+} //Εξω απο την while οταν εχει υπολογιστη το μηκος collatz του αριθμου το συγκρινουμε με το max και στο τελος εξω απο το for loop το τυπωνουμαι 
 }
 }
 
